daf9066f41405ac0e3d311306bf5a906e09f8b29
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-01-16 16:48:12 +0530

    waiting for 500 ms of idleness from user to check the form validity, using useEffect cleanp function.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const timeout = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return ()=>{  // cleanup function
+      clearTimeout(timeout)
+    }
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
